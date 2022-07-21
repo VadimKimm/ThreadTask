8b7c67639e8d15193482e5df58e9fc8a43ecfe60
Vadim <vadim_kimm@icloud.com>
2022-07-21 21:03:35 +0300

    Created class ChipStorage

--- IOS6-HW22-Kim Vadim.playground/Contents.swift
+++ IOS6-HW22-Kim Vadim.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class ChipStorage {
+    private var chipArray = [Chip]()
+    private let queue = DispatchQueue(label: "myQueue", qos: .utility, attributes: .concurrent)
+
+    func appendChip(_ value: Chip) {
+        queue.async(flags: .barrier) {
+            self.chipArray.append(value)
+        }
+    }
+
+    func getChip() -> Chip {
+        queue.sync {
+            return chipArray.removeLast()
+        }
+    }
+}
+
+let storage = ChipStorage()
+
+
