021656f5663db77729ece169390cca4ccd0299c3
Vadim <vadim_kimm@icloud.com>
2022-07-21 21:04:54 +0300

    Created class GeneratingThread

--- IOS6-HW22-Kim Vadim.playground/Contents.swift
+++ IOS6-HW22-Kim Vadim.playground/Contents.swift
@@ -17,6 +17,43 @@ class ChipStorage {
     }
 }
 
+class GeneratingThread: Thread {
+
+    private var storage: ChipStorage
+    private var seconds = 20
+    private var timer = Timer()
+    private var counter = 1
+
+    init(storage: ChipStorage) {
+        self.storage = storage
+    }
+
+    override func main() {
+        timer = Timer.scheduledTimer(timeInterval: 2,
+                                     target: self,
+                                     selector: #selector(runTimedCode),
+                                     userInfo: nil,
+                                     repeats: true)
+        RunLoop.current.add(timer, forMode: .common)
+        RunLoop.current.run()
+    }
+
+    @objc func runTimedCode() {
+        guard seconds > 0 else {
+            timer.invalidate()
+            self.cancel()
+            return
+        }
+        storage.appendChip(Chip.make())
+        print("Добавлена микросхема \(counter)")
+        seconds -= 2
+        counter += 1
+    }
+}
+
+
 let storage = ChipStorage()
 
+let generatingThread = GeneratingThread(storage: storage)
+generatingThread.start()
 
